b34b3479bf32e1f2b5a68cef208d7abeda27e625
Choy <choy98@outlook.com.pe>
2023-04-23 21:20:28 -0500

    feat: Genero constructores y metodos get y set de las clases food y order

--- sit-api-delivery/src/main/java/com/sitapidelivery/sitapidelivery/entities/Food.java
+++ sit-api-delivery/src/main/java/com/sitapidelivery/sitapidelivery/entities/Food.java
@@ -1,4 +1,36 @@
 package com.sitapidelivery.sitapidelivery.entities;
 
 public class Food {
+    private String name, description;
+    private double price;
+
+    public Food(String name, String description, double price) {
+        this.name = name;
+        this.description = description;
+        this.price = price;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public double getPrice() {
+        return price;
+    }
+
+    public void setPrice(double price) {
+        this.price = price;
+    }
 }
--- sit-api-delivery/src/main/java/com/sitapidelivery/sitapidelivery/entities/Order.java
+++ sit-api-delivery/src/main/java/com/sitapidelivery/sitapidelivery/entities/Order.java
@@ -1,4 +1,69 @@
 package com.sitapidelivery.sitapidelivery.entities;
 
+import java.time.LocalDateTime;
+import java.util.List;
+
 public class Order {
+    private static int count = 0, id;
+    private String customerName, customerEmail,status;
+    private LocalDateTime creationTime, estimatedDeliveryTime;
+    private List<Food> items;
+
+    public Order(String customerName, String customerEmail, String status, LocalDateTime creationTime, LocalDateTime estimatedDeliveryTime, List<Food> items) {
+        id= count++;
+        this.customerName = customerName;
+        this.customerEmail = customerEmail;
+        this.status = status;
+        this.creationTime = creationTime;
+        this.estimatedDeliveryTime = estimatedDeliveryTime;
+        this.items = items;
+    }
+
+    public String getCustomerName() {
+        return customerName;
+    }
+
+    public void setCustomerName(String customerName) {
+        this.customerName = customerName;
+    }
+
+    public String getCustomerEmail() {
+        return customerEmail;
+    }
+
+    public void setCustomerEmail(String customerEmail) {
+        this.customerEmail = customerEmail;
+    }
+
+    public String getStatus() {
+        return status;
+    }
+
+    public void setStatus(String status) {
+        this.status = status;
+    }
+
+    public LocalDateTime getCreationTime() {
+        return creationTime;
+    }
+
+    public void setCreationTime(LocalDateTime creationTime) {
+        this.creationTime = creationTime;
+    }
+
+    public LocalDateTime getEstimatedDeliveryTime() {
+        return estimatedDeliveryTime;
+    }
+
+    public void setEstimatedDeliveryTime(LocalDateTime estimatedDeliveryTime) {
+        this.estimatedDeliveryTime = estimatedDeliveryTime;
+    }
+
+    public List<Food> getItems() {
+        return items;
+    }
+
+    public void setItems(List<Food> items) {
+        this.items = items;
+    }
 }
